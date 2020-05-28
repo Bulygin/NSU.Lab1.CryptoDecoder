21f5d945d4682543b982b7ac609018f6573b268a
Bulygin <jbulygin@yandex.ru>
2020-05-28 17:28:28 +0300

    Finish task

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        String s = "123";
+        String st = new String("123");
+        System.out.println(s == st);
+        System.out.println(s);
+        System.out.println(st);
+    }
+}
+
